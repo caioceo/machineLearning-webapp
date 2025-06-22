189ef5cfaaaffadb3400ae9a34781f5b8e1cf2cb
cxio116 <caioceo06@gmail.com>
2025-06-22 17:32:33 -0300

    end point json

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from flask import Flask, request;
 from flask_cors import CORS;
 import pandas as pd;
 import io
-from models.XGBoost import xgboost
+import json
 from models.XGBoost import train_categorical_model
 
 main = Flask(__name__)
@@ -13,22 +13,16 @@ CORS(main)
 def generator():
     target = request.form.get("target")
     file = request.files["file"]
-    model = request.form.get("model")
-    targetValues = request.form.get("values") 
+    data = request.form.get("columns")
+    columns = json.loads(data)
     
     csvFile = file.read().decode("utf-8")
     
     df = pd.read_csv(io.StringIO(csvFile))
     
-    cols = df.columns.tolist()
-    
+    print(columns)
     print(target)
-    print(cols)
-    print (model)
-    
-    match model:
-        case "XGBoost":
-            results = train_categorical_model(df, target, cols)
+    results = train_categorical_model(df, target, columns)
 
     return "sucesso"
     
